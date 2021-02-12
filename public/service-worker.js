c4d06b7dc38f090b792225b70660d9803ef00e80
robertsicher <62100753+robertsicher@users.noreply.github.com>
2021-02-12 12:55:40 +0000

    Files to be cached

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,13 @@
 // Cached files
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/index.js",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "/manifest.webmanifest",
+    "/styles.css",
+  ];
 
 //create the cache
 
