be36a1d37d019421e31be21d88e670e490dd7694
robertsicher <62100753+robertsicher@users.noreply.github.com>
2021-02-14 17:53:20 +0000

    Create the cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,15 +1,30 @@
 // Cached files
 const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/index.js",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png",
-    "/manifest.webmanifest",
-    "/styles.css",
-  ];
+  "/",
+  "/index.html",
+  "/index.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "/manifest.webmanifest",
+  "/styles.css",
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
 
 //create the cache
+
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
 
 //Store API Calls
 
